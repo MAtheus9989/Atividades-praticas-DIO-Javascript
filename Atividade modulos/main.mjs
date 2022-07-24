a095a249689d0d4018825b30b95906a60fc26986
MAtheus9989 <matheusmattos2001@gmail.com>
2022-07-24 13:56:24 -0300

    Atividade modulo

---
+++ Atividade modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Matheus', 20));
+console.log(mostraCidade('Matheus', 'São PAulo'));
+console.log(mostraHobby('Matheus', 'Jogar basquete'));
